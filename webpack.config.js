93feaa97ce2a21d119fa515b36a67a0487a4ef80
Nick Kuria <nkuria01@gmail.com>
2021-05-21 22:58:25 +0300

    Finished hompage

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  watch: true,
+  watchOptions: {
+    aggregateTimeout: 200,
+    poll: 1000,
+    ignored: /node_modules/,
+  },
+  module: {
+    rules: [
+      // {
+      //   test: /\.css$/i,
+      //   use: ['style-loader', 'css-loader'],
+      // },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      },
+    ],
+  },
+};
