39cd36d01b38f4ca29cd04a1efebb338023c8a8c
Nick Kuria <nkuria01@gmail.com>
2021-05-22 11:41:10 +0300

    Comlplted pages

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 import './main.scss';
 
+var burger = document.getElementById('burger');
+var links = document.getElementById('links');
+var quit = document.getElementById('quit');
+
+function burgerC(){
+    burger.style.padding = '16px 16px 200vw 200vw';
+    links.style.display = 'flex';
+    quit.style.display = 'inline';
+  }
+
+burger.addEventListener('click', burgerC);
+  
+function quitC(){
+    burger.style.padding = '16px 16px 32px 32px';
+    links.style.display = 'none';
+    quit.style.display = 'none';
+}
+
+quit.addEventListener('click', quitC);
+
